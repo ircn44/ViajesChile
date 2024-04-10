c645e5deb310bb0edbdbd6787856e0cabb0fa7af
Nicolas Arancibia <nj.arancibiah@gmail.com>
2024-04-09 23:26:09 -0400

    órden y smooth scroll

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,3 +7,19 @@ $("h5#cartastitulo.card-title").on("click", function() {
     $("p.card-text").toggle();
 
 })
+
+
+$(document).ready(function(){
+ $("a").on('click', function(event) {
+ if (this.hash !== "") {
+ event.preventDefault();
+ var hash = this.hash;
+ $('html, body').animate({
+ scrollTop: $(hash).offset().top
+ }, 800, function(){
+
+ window.location.hash = hash;
+ });
+ }
+ });
+});
