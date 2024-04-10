f49a8b0e9c2898981f07a7e70a39ab7711ae98d1
Nicolas Arancibia <nj.arancibiah@gmail.com>
2024-04-09 21:56:54 -0400

    agregado toggle a las cards

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,3 +2,8 @@ $("#botonenviar").on("click", function() {
     alert("Mensaje enviado correctamente");
     
 })
+
+$("h5#cartastitulo.card-title").on("click", function() {
+    $("p.card-text").toggle();
+
+})
